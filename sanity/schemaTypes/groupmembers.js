69b3e5f5b08526270260c03dd57e05d0cb073cab
kevinlj2 <kevlok4@gmail.com>
2025-04-08 12:24:05 +0200

    Lagt inn medlemmer

---
+++ sanity/schemaTypes/groupmembers.js
@@ -0,0 +1,23 @@
+export const groupmembers = {
+  name: 'groupmembers',
+  title: 'Members',
+  type: 'document',
+  fields: [
+    {
+      name: 'Navn',
+      type: 'string',
+    },
+    {
+      name: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'Interesser',
+      type: 'string',
+    },
+    {
+      name: 'Biografi',
+      type: 'string',
+    },
+  ],
+}
--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import {groupmembers} from './groupmembers'
+
+export const schemaTypes = [groupmembers]
