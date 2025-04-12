cd0a2e6ced37b5add579a5af40a3835e453f8eee
kevinlj2 <kevlok4@gmail.com>
2025-04-12 14:49:53 +0200

    Fikset clienten

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -1,25 +1,14 @@
-import { SanityClient } from "@sanity/client";
+import { createClient } from "@sanity/client";
 
 const options = {
   projectId: "f6809pwb",
   dataset: "production",
 };
 
-const client = SanityClient({
+const client = createClient({
   ...options,
-  apiVersion: "v2025-04-08",
-  useCdn: true,
+  apiVersion: "2025-04-08",
+  useCdn: false,
 });
 
 export default client;
-
-export const getHeaderData = async () => {
-  const query = `*[_type == "members"][0]{
-  Name,
-  Epost,
-  Intrests,
-  Biografi,
-  Bilde
-}`;
-  return await client.fetch(query);
-};
