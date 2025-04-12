fadf686725da6afd6c571df66ea7d9f3731752cc
kevinlj2 <kevlok4@gmail.com>
2025-04-12 14:51:24 +0200

    Lagt inn personene på gruppen

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,3 +1,31 @@
-export default function PersonCard() {
-  return;
-}
+import { useEffect, useState } from "react";
+import { getGroupMembers } from "../sanity/groupindex";
+
+const PersonCard = () => {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getGroupMembers();
+      setMembers(data || []);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {members.map((member, index) => (
+        <div key={index}>
+          <h2>{member.Navn}</h2>
+          <p>{member.Epost}</p>
+          {member.BildeUrl && (
+            <img src={member.BildeUrl} alt={member.Name} width="200" />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PersonCard;
