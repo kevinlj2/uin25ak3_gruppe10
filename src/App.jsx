52b9968dd026aad774d6b26be6f8e08903d5466e
kevinlj2 <kevlok4@gmail.com>
2025-04-12 14:52:44 +0200

    Lagt inn basic oppsett

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/Layout";
+import Layout from "./components/layout";
 
 function App() {
-  return;
-  <Layout />;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="" />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
