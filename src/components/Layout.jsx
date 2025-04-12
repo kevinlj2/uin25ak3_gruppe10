a5b7dc3fc4aaf4318616289ea04e59c1f8fcb017
kevinlj2 <kevlok4@gmail.com>
2025-04-12 14:52:03 +0200

    oppdatert

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,17 @@
+import Header from "./Header";
+import Logg from "./Logg";
+import PersonCard from "./PersonCard";
+
 export default function Layout() {
-  return;
+  return (
+    <>
+      <Header />
+      <section>
+        <PersonCard />
+      </section>
+      <section>
+        <Logg />
+      </section>
+    </>
+  );
 }
