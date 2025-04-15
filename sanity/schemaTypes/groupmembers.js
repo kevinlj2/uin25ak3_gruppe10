19ace98812af10add33a14b1c904a8a7ff0008bc
kevinlj2 <kevlok4@gmail.com>
2025-04-15 16:13:04 +0200

    Lagt inn slugifier

--- sanity/schemaTypes/groupmembers.js
+++ sanity/schemaTypes/groupmembers.js
@@ -23,5 +23,22 @@ export const groupmembers = {
       name: 'Bilde',
       type: 'image',
     },
+    {
+      name: 'memberslug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'members',
+        maxLength: 200,
+        slugify: (input) =>
+          input
+            .toLowerCase()
+            .replace(/\s+/g, '-')
+            .replace('æ', 'ae')
+            .replace('å', 'aa')
+            .replace('ø', 'o')
+            .slice(0, 200),
+      },
+    },
   ],
 }
