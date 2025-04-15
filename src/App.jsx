a97b7358b8fee35e22dbea134a4795886e182549
kevinlj2 <kevlok4@gmail.com>
2025-04-15 16:48:02 +0200

    Satt opp routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
+import PersonPage from "./components/PersonPage";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="" />
+        <Route path="/" element={<Home />} />
+        <Route path="/medlem/:slug" element={<PersonPage />} />
       </Routes>
     </Layout>
   );
