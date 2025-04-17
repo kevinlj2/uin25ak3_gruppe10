534fc1c6f7c3e792354a00f79cf220d0e330db2b
kevinlj2 <kevlok4@gmail.com>
2025-04-17 20:03:20 +0200

    Lagt inn styling for PersonPage

--- src/components/PersonPage.jsx
+++ src/components/PersonPage.jsx
@@ -1,10 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getGroupMembers, getWorklog } from "../sanity/groupindex";
+import "../styles/_personpage.scss";
 
 export default function PersonPage() {
   const { slug } = useParams();
-
   const [medlem, setMedlem] = useState(null);
   const [logs, setLogs] = useState([]);
 
@@ -28,20 +28,31 @@ export default function PersonPage() {
   if (!medlem) return <p>Laster medlem...</p>;
 
   return (
-    <>
-      <h1>{medlem.Navn}</h1>
-      <p>Interesser: {medlem.Interesser}</p>
-      <p>{medlem.Biografi}</p>
-      <img src={medlem.BildeUrl} alt={medlem.Navn} width="200" />
-      <h2>Arbeidslogg</h2>
-      {logs.map((log) => (
-        <div key={log.lognumber}>
-          <p>{log.dato}</p>
-          <p>{log.memberlog}</p>
-          <p>{log.logs}</p>
-          <p>{log.hoursspent} timer</p>
+    <section className="personpage">
+      <div className="person-header">
+        <img src={medlem.BildeUrl} alt={medlem.Navn} className="person-bilde" />
+        <div className="person-info">
+          <h1>{medlem.Navn}</h1>
+          <p>{medlem.Biografi}</p>
+
+          <h2>Interesser</h2>
+          <ul>
+            <li>{medlem.Interesser}</li>
+          </ul>
         </div>
-      ))}
-    </>
+      </div>
+
+      <section className="log">
+        <h2>Arbeidslogg</h2>
+        {logs.map((log) => (
+          <div key={log.lognumber}>
+            <p>{log.dato}</p>
+            <p>{log.memberlog}</p>
+            <p>{log.logs}</p>
+            <p>{log.hoursspent} timer</p>
+          </div>
+        ))}
+      </section>
+    </section>
   );
 }
